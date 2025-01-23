0f7f6bfa664ef2447b4214d9167bf88a68272cad
Manwendra Kishore <manwendra.kishore@bjss.com>
2025-01-23 12:26:52 +0000

    JIRA-0054 : Package separated

--- goLangToDoApp/main.go
+++ goLangToDoApp/main.go
@@ -1,24 +1,16 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
-	"os"
+	"goLangToDoApp/toDoUtil"
 )
 
-const fileName = "./GoLangToDoApp.json"
-
-type ToDoItem struct {
-	ItemId      int    `json:"id"`
-	Header      string `json:"header"`
-	Description string `json:"description"`
-}
+const fileName = "./toDoUtil/ToDoAppData.json"
 
 func main() {
 	fmt.Println("==================== Welcome to Manwendra's TO-DO List Application. =======================")
-	PrintFlagInstructions()
+	toDoUtil.PrintFlagInstructions()
 
 	list := flag.Bool("list", false, "List all To-Do Items in List")
 	add := flag.Bool("add", false, "Add a new To-Do Item to List")
@@ -33,7 +25,7 @@ func main() {
 	flag.Parse()
 
 	// Load All To-Do Items from file
-	items := GetAllToDoItems(fileName)
+	items := toDoUtil.GetAllToDoItems(fileName)
 	if items == nil {
 		fmt.Println("============================== No To-Do Items in the List =================================")
 	}
@@ -41,124 +33,37 @@ func main() {
 	// List all the To-Do Items
 	if *list {
 		if items != nil {
-			PrintToDoItems(items)
+			toDoUtil.PrintToDoItems(items)
 		}
 	}
 
 	// Add a new To-Do Item
 	if *add {
-		items = AddNewToDoItem(items, *header, *desc)
+		items = toDoUtil.AddNewToDoItem(items, *header, *desc)
 		fmt.Println("=========================== New To-Do Item added to the List ==============================")
 	}
 
 	// Update a To-Do Item
 	if *update && *id != 0 {
-		items = UpdateToDoItem(items, ToDoItem{*id, *header, *desc})
+		items = toDoUtil.UpdateToDoItem(items, *id, *header, *desc)
 		fmt.Println("================================== To-Do Item updated ====================================")
 	}
 
 	// Delete a To-Do Item
 	if *remove && *id != 0 {
-		items = RemoveToDoItem(items, *id)
-		fmt.Println("=================================== To-Do Item removed ====================================")
+		items = toDoUtil.RemoveToDoItem(items, *id)
+		fmt.Println("=================================== To-Do Item deleted ====================================")
 	}
 
 	// Delete all To-Do Item(s)
 	if *removeAll {
 		items = nil
-		fmt.Println("================================ All To-Do Item(s) removed ================================")
+		fmt.Println("================================ All To-Do Item(s) deleted ================================")
 	}
 
 	// Save All To-Do Items to file
-	err := SaveAllToDoItems(items)
+	err := toDoUtil.SaveAllToDoItems(items, fileName)
 	if err != nil {
 		fmt.Println("==========> Error saving file:", fileName, err)
 	}
-}
-
-func PrintFlagInstructions() {
-	fmt.Println("======================== Use following flags for various operations =======================")
-	fmt.Println("-list to \"Print all To-Do Items in the List\"")
-	fmt.Println("-add -header=<name> -desc <description> to \"Add a new To-Do Item\"")
-	fmt.Println("-update -id=<itemId> -header=<name> -desc <description> to \"Update a To-Do Item\"")
-	fmt.Println("-remove -id=<itemId> to \"Delete a To-Do Item\"")
-	fmt.Println("-removeAll to \"Delete all To-Do Items\"")
-	fmt.Println("===========================================================================================")
-}
-
-func PrintToDoItems(items []ToDoItem) {
-	fmt.Println("================================== Your To-Do Task Items ==================================")
-	//fmt.Println(items)
-	for index, item := range items {
-		if index != 0 {
-			fmt.Println("-------------------------------------------------------------------------------------------")
-		}
-		fmt.Printf("%d. %s\n", item.ItemId, item.Header)
-		fmt.Println(item.Description)
-	}
-	fmt.Println("===========================================================================================")
-}
-
-func AddNewToDoItem(currentItems []ToDoItem, header string, desc string) []ToDoItem {
-	id := 1
-	itemNos := len(currentItems)
-	if itemNos > 0 {
-		id = currentItems[itemNos-1].ItemId + 1
-	}
-	return append(currentItems, ToDoItem{id, header, desc})
-}
-
-func UpdateToDoItem(currentItems []ToDoItem, updateItem ToDoItem) []ToDoItem {
-	for index, item := range currentItems {
-		if item.ItemId == updateItem.ItemId {
-			currentItems[index].Header = updateItem.Header
-			currentItems[index].Description = updateItem.Description
-		}
-	}
-	return currentItems
-}
-
-func RemoveToDoItem(currentItems []ToDoItem, deleteItemId int) []ToDoItem {
-	for index, item := range currentItems {
-		if item.ItemId == deleteItemId {
-			currentItems = append(currentItems[:index], currentItems[index+1:]...)
-		}
-	}
-	return currentItems
-}
-
-func SaveAllToDoItems(allItems []ToDoItem) error {
-	// Open json file
-	data, err := json.MarshalIndent(allItems, "", "\t")
-	if err != nil {
-		return err
-	}
-	return ioutil.WriteFile(fileName, data, 0644)
-}
-
-func GetAllToDoItems(fileName string) []ToDoItem {
-	// Open local json file
-	jsonFile, err := os.Open(fileName)
-	if err != nil {
-		fmt.Println("==========> Error Opening file:", fileName, err)
-	} else {
-		byteValue, err := ioutil.ReadAll(jsonFile)
-		if err != nil {
-			fmt.Println("==========> Error Reading file:", fileName, err)
-		} else {
-			if byteValue != nil {
-				// Parse the json file to ToDoItems
-				var items []ToDoItem
-				err := json.Unmarshal(byteValue, &items)
-				if err != nil {
-					fmt.Println("==========> Error Unmarshalling file:", err)
-				} else {
-					if items != nil {
-						return items
-					}
-				}
-			}
-		}
-	}
-	return nil
 }
---
+++ goLangToDoApp/toDoUtil/toDoUtil.go
@@ -0,0 +1,101 @@
+package toDoUtil
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+type ToDoItem struct {
+	ItemId      int    `json:"id"`
+	Header      string `json:"header"`
+	Description string `json:"description"`
+}
+
+func PrintFlagInstructions() {
+	fmt.Println("======================== Use following flags for various operations =======================")
+	fmt.Println("-list												to \"Print all To-Do Items in the List\"")
+	fmt.Println("-add -header=<name> -desc <description> 						 to \"Add a new To-Do Item\"")
+	fmt.Println("-update -id=<itemId> -header=<name> -desc <description> 		  to \"Update a To-Do Item\"")
+	fmt.Println("-remove -id=<itemId> 											  to \"Delete a To-Do Item\"")
+	fmt.Println("-removeAll													 to \"Delete all To-Do Item(s)\"")
+	fmt.Println("===========================================================================================")
+}
+
+func PrintToDoItems(items []ToDoItem) {
+	fmt.Println("================================== Your To-Do Task Items ==================================")
+	//fmt.Println(items)
+	for index, item := range items {
+		if index != 0 {
+			fmt.Println("-------------------------------------------------------------------------------------------")
+		}
+		fmt.Printf("%d. %s\n", item.ItemId, item.Header)
+		fmt.Println(item.Description)
+	}
+	fmt.Println("===========================================================================================")
+}
+
+func AddNewToDoItem(currentItems []ToDoItem, header string, desc string) []ToDoItem {
+	id := 1
+	itemNos := len(currentItems)
+	if itemNos > 0 {
+		id = currentItems[itemNos-1].ItemId + 1
+	}
+	return append(currentItems, ToDoItem{id, header, desc})
+}
+
+func UpdateToDoItem(currentItems []ToDoItem, id int, header string, desc string) []ToDoItem {
+	for index, item := range currentItems {
+		if item.ItemId == id {
+			currentItems[index].Header = header
+			currentItems[index].Description = desc
+		}
+	}
+	return currentItems
+}
+
+func RemoveToDoItem(currentItems []ToDoItem, deleteItemId int) []ToDoItem {
+	for index, item := range currentItems {
+		if item.ItemId == deleteItemId {
+			currentItems = append(currentItems[:index], currentItems[index+1:]...)
+		}
+	}
+	return currentItems
+}
+
+func SaveAllToDoItems(allItems []ToDoItem, fileName string) error {
+	// Open json file
+	data, err := json.MarshalIndent(allItems, "", "\t")
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(fileName, data, 0644)
+}
+
+func GetAllToDoItems(fileName string) []ToDoItem {
+	// Open local json file
+	jsonFile, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println("==========> Error Opening file:", fileName, err)
+	} else {
+		byteValue, err := ioutil.ReadAll(jsonFile)
+		if err != nil {
+			fmt.Println("==========> Error Reading file:", fileName, err)
+		} else {
+			if byteValue != nil {
+				// Parse the json file to ToDoItems
+				var items []ToDoItem
+				err := json.Unmarshal(byteValue, &items)
+				if err != nil {
+					fmt.Println("==========> Error Unmarshalling file:", err)
+				} else {
+					if items != nil {
+						return items
+					}
+				}
+			}
+		}
+	}
+	return nil
+}
